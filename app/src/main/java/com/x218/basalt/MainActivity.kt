c8ae0b0af99c92f37291ad43b241b90d9e96af69
x218 <rayyanrayyanls50@gmail.com>
2026-02-05 22:59:01 +0300

    NEW !!! Main Activity

--- app/src/main/java/com/x218/basalt/MainActivity.kt
+++ app/src/main/java/com/x218/basalt/MainActivity.kt
@@ -1,143 +1,67 @@
 package com.x218.basalt
 
-import android.Manifest
+import android.annotation.SuppressLint
+import android.hardware.SensorManager
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
-
-import androidx.core.app.ActivityCompat
-
-import android.content.pm.PackageManager
-import android.location.Location
-
-import android.location.LocationListener
-import android.location.LocationManager
-import android.location.LocationManager.GPS_PROVIDER
-import android.os.Build
-import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.contract.ActivityResultContracts
 import com.x218.basalt.data.PermissionState
+import com.x218.basalt.data.checkPermissions
+import com.x218.basalt.data.checkGpsProvider
+import com.x218.basalt.data.getAzimuth
+import com.x218.basalt.data.getLocation
+import com.x218.basalt.data.initializeSensorData
+import com.x218.basalt.data.requestPermission
 import com.x218.basalt.ui.MainScreen
-import com.x218.basalt.ui.theme.MyApplicationTheme
-import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
+const val TAG: String = "MainActivity"
+
 class MainActivity : ComponentActivity() {
 
-    class MyLocationListener : LocationListener {
-        val mContext: MainActivity
-
-        constructor(mContext: MainActivity) {
-            this.mContext = mContext
-        }
-
-        override fun onLocationChanged(location: Location) {
-            mContext.updateLocation(location)
-        }
-    }
-
-    // Permissions
-    val perms = PermissionState(coarse = false, fine = false)
-    val compass = CompassState(kaabaBearing = 0.0f, north = 0.0f)
-    val kaabaLocation = Location(GPS_PROVIDER)
-
-    val showRationale =
-	    ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION) ||
-	    ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-
-    // Activity Result Launcher
-    val locationPermissionRequest = registerForActivityResult(
-        ActivityResultContracts.RequestMultiplePermissions()
-    ) { permissions ->
-        when {
-            permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
-		        // fine location granted
-                perms.fine = true
-            }
-            permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
-		        // only coarse location granted
-		        perms.coarse = true
-            }
-            else -> {
-		        // No location granted, show error
-                perms.coarse = false
-                perms.fine = false
-            }
-        }
-    }
-
-    // val locationManager: LocationManager? = null
-    val locationListener: LocationListener = MyLocationListener(this)
-    val locationManager = this.getSystemService(LOCATION_SERVICE) as LocationManager
-
+    @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
-	    kaabaLocation.latitude = 21.2445
-        kaabaLocation.longitude = 39.82617
-
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            requestPermission()
-        }
-        locationManager.getLastKnownLocation(GPS_PROVIDER)
-        locationManager.requestLocationUpdates(GPS_PROVIDER, 1000, 0.0f, locationListener)
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            when {
-                !locationManager.hasProvider(GPS_PROVIDER) -> {
-                    // No GPS error
-                }
-                !locationManager.isProviderEnabled(GPS_PROVIDER) -> {
-                    // GPS disabled
-                }
-                !locationManager.isLocationEnabled -> {
-                    // Location disabled
-                }
-            }
-        }
-
-        enableEdgeToEdge()
-
         super.onCreate(savedInstanceState)
-        setContent {
-            MainScreen(perms, compass, kaabaLocation)
-            MyApplicationTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    MainScreen(perms, compass, kaabaLocation)
-                }
-            }
+
+        val lm = this.getSystemService(LOCATION_SERVICE) as LocationManager
+        val sm = this.getSystemService(SENSOR_SERVICE) as SensorManager
+        Log.i(TAG, "LocationManager: $lm , SensorManager: $sm")
+
+        val perms = checkPermissions(this)
+        Log.i(TAG, "Checked permissions: perms $perms")
+
+        if( perms == PermissionState(false, false)) {
+            requestPermission(perms, this)
         }
-    }
+        Log.i(TAG, "Obtained permissions $perms")
+        checkGpsProvider(lm)
 
-    fun updateLocation(location: Location) {
-	    this.compass.kaabaBearing = location.bearingTo(kaabaLocation)
-    }
+        val location = getLocation(lm, this)
+        Log.i(TAG, "Obtained location $location")
 
-    fun requestPermission() {
-        when {
-            // Show rationale dialog if needed
-            showRationale -> {}
-            else -> {
-                // launch permission request
-                this.locationPermissionRequest.launch(
-                    arrayOf(
-                        Manifest.permission.ACCESS_FINE_LOCATION,
-                        Manifest.permission.ACCESS_COARSE_LOCATION
-                    )
-                )
-            }
+        val sObj = initializeSensorData(sm)
+        val northAngle = getAzimuth(sObj)
+        Log.i(TAG, "Sensor data $sObj , $northAngle")
+
+        val kaabaLocation = Location(LocationManager.GPS_PROVIDER).apply {
+            latitude = 21.2445
+            longitude = 39.82617
+        }
+        Log.println(Log.INFO, "MainActivity", "kaaba location: $kaabaLocation")
+
+        val kaabaDirection = location.bearingTo(kaabaLocation)
+        Log.println(Log.INFO, "MainActivity", "Kaaba direction : $kaabaDirection")
+
+        Log.println(Log.INFO, "MainActivity", "Completed all setup")
+
+        setContent {
+            MainScreen(
+                perms,
+                CompassState(kaabaDirection, northAngle),
+                location
+            )
         }
     }
 }
