feea770fb6fe87db5da604afdaa93d6673beef73
x218 <rayyanrayyanls50@gmail.com>
2026-02-08 22:18:37 +0300

    Implement header as top bar

--- app/src/main/java/com/x218/basalt/ui/components/header.kt
+++ app/src/main/java/com/x218/basalt/ui/components/header.kt
@@ -9,10 +9,18 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.PlainTooltip
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TooltipAnchorPosition
+import androidx.compose.material3.TooltipBox
+import androidx.compose.material3.TooltipDefaults
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberTooltipState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -23,41 +31,49 @@ import androidx.compose.ui.unit.dp
 import com.x218.basalt.R
 
 //@Preview
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Header() {
-    Row(
-        modifier = Modifier.padding(16.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.app_logo),
-            modifier = Modifier.size(36.dp),
-            contentDescription = stringResource(id = R.string.app_logo_description)
-        )
-
-        Spacer(
-            modifier = Modifier.width(4.dp)
-        )
-
-        Text(
-            text = stringResource(R.string.app_name),
-            style = MaterialTheme.typography.titleLarge
-        )
-
-        Spacer(
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        // Help Icon
-        IconButton(
-            onClick = { print("Info Button clicked") }
-        ) {
-            Icon(
-                imageVector = Icons.Filled.Info,
-                contentDescription = stringResource(R.string.info_desc)
+fun Header(
+    onClickInfo: () -> Unit = {},
+    modifier: Modifier = Modifier
+) {
+    val padding = 4.dp
+    TopAppBar(
+        title = {
+            Text(
+                modifier = Modifier.padding(padding),
+                text = stringResource(R.string.app_name),
+                style = MaterialTheme.typography.titleLarge
             )
+        },
+        navigationIcon = {
+            Image(
+                painter = painterResource(id = R.drawable.rose),
+                modifier = Modifier
+                    .size(60.dp)
+                    .padding(padding),
+                contentDescription = stringResource(id = R.string.app_logo_description)
+            )
+        },
+        actions = {
+            TooltipBox(
+                modifier = modifier,
+                positionProvider = TooltipDefaults.rememberTooltipPositionProvider(
+                    TooltipAnchorPosition.Below),
+                tooltip = {
+                    PlainTooltip { Text("Info") }
+                },
+                state = rememberTooltipState()
+            ) {
+                IconButton(onClick = { /* Do something... */ }) {
+                    Icon(
+                        imageVector = Icons.Filled.Info,
+                        contentDescription = "Add to favorites"
+                    )
+                }
+            }
         }
-    }
+    )
 }
 
 @Preview
