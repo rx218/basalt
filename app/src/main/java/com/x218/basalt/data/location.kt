f7957033c9c8bf992617d332f707e8b6d9d347c6
x218 <rayyanrayyanls50@gmail.com>
2026-02-14 10:50:19 +0300

    Add conditional logic to select available location provider

--- app/src/main/java/com/x218/basalt/data/location.kt
+++ app/src/main/java/com/x218/basalt/data/location.kt
@@ -1,7 +1,6 @@
 package com.x218.basalt.data
 
 import android.Manifest
-import android.content.Context
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
@@ -16,10 +15,16 @@ import kotlin.coroutines.suspendCoroutine
 // getLocation may take a long time to complete, so it is preferred to invoke it as a coroutine
 @RequiresPermission(allOf = [Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION])
 suspend fun getLocation(lm: LocationManager, exec: Executor): Location {
-    // See if we have a cached Location
-    if (lm.getLastKnownLocation(LocationManager.GPS_PROVIDER) != null) {
-        return lm.getLastKnownLocation(LocationManager.GPS_PROVIDER)!!
+    val provider = when {
+        Build.VERSION.SDK_INT >= Build.VERSION_CODES.S &&
+                lm.isProviderEnabled(LocationManager.FUSED_PROVIDER) -> LocationManager.FUSED_PROVIDER
+        lm.isProviderEnabled(LocationManager.PASSIVE_PROVIDER) -> LocationManager.PASSIVE_PROVIDER
+        lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER) -> LocationManager.NETWORK_PROVIDER
+        else -> LocationManager.GPS_PROVIDER
     }
+
+    // See if we have a cached Location
+    lm.getLastKnownLocation(provider)?.let { return it }
 
     // Otherwise get a new copy
     // suspendCoroutine waits until the respective callbacks are executed
@@ -27,10 +32,11 @@ suspend fun getLocation(lm: LocationManager, exec: Executor): Location {
         // For android versions above R, use getCurrentLocation
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             lm.getCurrentLocation(
-                LocationManager.GPS_PROVIDER,
+                provider,
                 null,
                 exec,
                 { l: Location ->
+                    Log.i("getLocation", "Using getCurrentLocation")
                     continuation.resume(l)
                 }
             )
@@ -38,12 +44,13 @@ suspend fun getLocation(lm: LocationManager, exec: Executor): Location {
         // For older versions of android use Location Listener
         else {
             lm.requestLocationUpdates(
-                LocationManager.GPS_PROVIDER,
+                provider,
                 0,
                 0f,
                 object: LocationListener {
                     override fun onLocationChanged(l: Location) {
                         lm.removeUpdates(this)
+                        Log.i("getLocation", "Using requestLocationUpdates")
                         continuation.resume(l)
                     }
                 }
@@ -66,6 +73,8 @@ fun checkGpsProvider(lm: LocationManager): Boolean {
     } else {
         locationEnabled= true
     }
+
+    Log.i("checkGpsProvider", "locationEnabled: $locationEnabled and hasGPS = $hasGPS")
 
     return locationEnabled && hasGPS
 }
