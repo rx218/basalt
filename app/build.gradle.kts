460090b8f3087490028fda926faee9487f37f531
rx218 <129903889+rx218@users.noreply.github.com>
2026-02-19 22:53:46 +0300

    Refactor

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,21 +76,9 @@ dependencies {
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
     // Tooling
     debugImplementation(libs.androidx.compose.ui.tooling)
-    // Instrumented tests
-    // androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-    // debugImplementation(libs.androidx.compose.ui.test.manifest)
-
-    // Local tests: jUnit, coroutines, Android runner
-    // testImplementation(libs.junit)
-    // testImplementation(libs.kotlinx.coroutines.test)
-
-    // Instrumented tests: jUnit rules and runners
-
-    // androidTestImplementation(libs.androidx.test.core)
-    // androidTestImplementation(libs.androidx.test.ext.junit)
-    // androidTestImplementation(libs.androidx.test.runner)
 
     // Adhan times library
     implementation(libs.adhan2)
